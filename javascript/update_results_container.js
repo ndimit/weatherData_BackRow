d65d58630b5659f4c7eb4de0a805b6acc994881c
unknown <bblomberg@LAB-ECB176-32.cvtc.local>
2013-05-07 17:52:52 -0500

    Updated the JS file so that the City textbox drops down as the user starts typing a city

--- javascript/update_results_container.js
+++ javascript/update_results_container.js
@@ -50,5 +50,38 @@ $(function() {
   var isNaI = function(n) {
 	return n % 1 !== 0;
   };
+  
+  var input = document.getElementById('city');
+  var options = {
+    types: ['(cities)'],
+    componentRestrictions: {country: 'us'}
+  };
+
+  autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      resetAndAddToResults("Please enter a valid 5 digit zip code");
+      return;
+    }
+	lat = place.geometry.location.kb;
+	lon = place.geometry.location.lb;
+    apiKey = "e2dbaf95fd927995";
+    url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/g/" + lat + "/" + lon + "/";
+    $.ajax({
+      url: url,
+      success: function(data) {
+        var jData = JSON.parse(data);
+		resetAndAddToHeader(jData.current_observation.display_location.city);
+		addToResults("Current Temperature: " + jData.current_observation.temp_f);
+		addToResults("Feels Like: " + jData.current_observation.feelslike_string);
+      },
+      error: function() {
+        console.log("Error");
+      }
+    });
+  });
+
 
 });
